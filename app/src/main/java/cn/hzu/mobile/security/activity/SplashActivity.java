eef5b322c838272e92e8f318ab938555cc1c4291
ivoth <ivoth@sina.com>
2017-04-20 16:11:33 +0800

    bug

--- app/src/main/java/cn/hzu/mobile/security/activity/SplashActivity.java
+++ app/src/main/java/cn/hzu/mobile/security/activity/SplashActivity.java
@@ -1,27 +1,80 @@
 package cn.hzu.mobile.security.activity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
+import cn.hzu.mobile.security.MainActivity;
 import cn.hzu.mobile.security.R;
 import cn.hzu.mobile.security.utils.StreamUtil;
 
 
 public class SplashActivity extends AppCompatActivity {
     private static final String TAG = "SplashActivity";
-    private TextView mVersionName;
+    /**
+     * 更新新版本的状态码
+     */
+    protected static final int UPDATE_VERSION = 100;
+    /**
+     * 进入应用程序主界面状态码
+     */
+    protected static final int ENTER_HOME = 101;
+
+    /**
+     * url地址出错状态码
+     */
+    protected static final int URL_ERROR = 102;
+    //以下是其他各种出错状态码
+    protected static final int IO_ERROR = 103;
+    protected static final int JSON_ERROR = 104;
+
+    private Handler mHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            switch (msg.what) {
+                case UPDATE_VERSION:
+                    showUpdateDialog();
+                    break;
+                case ENTER_HOME:
+                    enterHome();
+                    break;
+                case URL_ERROR:
+                    Toast.makeText(getApplicationContext(), "检查更新失败!", Toast.LENGTH_SHORT).show();
+                    enterHome();
+                    break;
+                case IO_ERROR:
+                    Toast.makeText(getApplicationContext(), "读取异常!", Toast.LENGTH_SHORT).show();
+                    enterHome();
+                    break;
+                case JSON_ERROR:
+
+                    break;
+            }
+        }
+    };
+
+    private void enterHome() {
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        finish();
+    }
+
+    private Bean bean;
+    private int mLocalVersionCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +85,9 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     private void init() {
-        mVersionName = (TextView) findViewById(R.id.tv_version);
-        mVersionName.setText("版本号: " + getVersionName());
-        int code = getVersionCode();
+        TextView versionName = (TextView) findViewById(R.id.tv_version);
+        versionName.setText("版本号: " + getVersionName());
+        mLocalVersionCode = getVersionCode();
         checkVersion();
     }
 
@@ -42,26 +95,47 @@ public class SplashActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
+                Message msg = new Message();
+                long startTime = System.currentTimeMillis();
                 try {
                     //设置url
-                    //TODO 渣渣出错
                     URL url = new URL("http://192.168.172.99:8080/update.json");
                     //开启链接
                     HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                     //连接超时
                     urlConnection.setConnectTimeout(2000);
                     urlConnection.setReadTimeout(2000);
-                    Log.i(TAG, "run: "+urlConnection.getResponseCode());
+                    Log.i(TAG, "run: " + urlConnection.getResponseCode());
                     if (urlConnection.getResponseCode() == 200) {
                         InputStream inputStream = urlConnection.getInputStream();
                         String string = StreamUtil.streamToString(inputStream);
+                        Gson gson = new Gson();
+                        bean = gson.fromJson(string, Bean.class);
+                        if (mLocalVersionCode < bean.getVersionCode()) {
+                            //提示用户更新,弹出对话框(UI),消息机制
+                            msg.what = UPDATE_VERSION;
+                        } else {
+                            //进入应用程序主界面
+                            msg.what = ENTER_HOME;
+                        }
                         Log.i(TAG, "checkVersion: " + string);
                     } else {
-//                        Toast.makeText(getApplicationContext(), "检查更新失败!", Toast.LENGTH_SHORT).show();
+                        msg.what = URL_ERROR;
                     }
                 } catch (IOException e) {
+                    msg.what = IO_ERROR;
                     e.printStackTrace();
+                } finally {
+                    long endTime = System.currentTimeMillis();
+                    if(endTime - startTime < 4000) {
+                        try {
+                            Thread.sleep(4000 - (endTime - startTime));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
+                mHandler.sendMessage(msg);
             }
         }).start();
     }
@@ -91,5 +165,63 @@ public class SplashActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return null;
+    }
+    /**
+     * 弹出对话框,提示用户更新
+     */
+    protected void showUpdateDialog() {
+        //对话框,是依赖于activity存在的
+        AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(getApplicationContext());
+        //设置左上角图标
+        builder.setIcon(R.mipmap.ic_launcher);
+        builder.setTitle("发现新版本");
+        //设置描述内容
+        builder.setMessage(bean.getDesc());
+        //积极按钮,立即更新
+        builder.setPositiveButton("立即更新", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //下载apk,apk链接地址,downloadUrl
+//                downloadApk();
+            }
+        });
+        builder.setNegativeButton("稍后再说", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //取消对话框,进入主界面
+                enterHome();
+            }
+        });
+        builder.show();
+    }
+    private class Bean {
+
+        /**
+         * versionName : 3
+         * versionCode : 10
+         * desc : 这是新版本,欢迎下载使用
+         * downloadUrl : http://www.baidu.com
+         */
+
+        private String versionName;
+        private int versionCode;
+        private String desc;
+        private String downloadUrl;
+        public String getVersionName() {
+            return versionName;
+        }
+
+        public int getVersionCode() {
+            return versionCode;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public String getDownloadUrl() {
+            return downloadUrl;
+        }
     }
 }
